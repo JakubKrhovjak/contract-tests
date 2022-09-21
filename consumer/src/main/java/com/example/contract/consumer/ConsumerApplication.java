563fad9184666d943e97a7c08480608d63dd54a5
Jakub Krhovjak <jakub.krhovjak@protonmail.com>
2022-09-21 16:20:21 +0200

    init

---
+++ consumer/src/main/java/com/example/contract/consumer/ConsumerApplication.java
@@ -0,0 +1,15 @@
+package com.example.contract.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConsumerApplication {
+
+     public static void main(String[] args) {
+           SpringApplication.run(ConsumerApplication.class, args);
+    }
+
+}
