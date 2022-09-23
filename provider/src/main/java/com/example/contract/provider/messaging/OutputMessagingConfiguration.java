a005ca2ccaf498b9ec7eacf8eca69e634e722e78
Jakub Krhovjak <jakub.krhovjak@protonmail.com>
2022-09-23 11:31:53 +0200

    step

--- provider/src/main/java/com/example/contract/provider/messaging/OutputMessagingConfiguration.java
+++ provider/src/main/java/com/example/contract/provider/messaging/OutputMessagingConfiguration.java
@@ -27,13 +27,6 @@ public class OutputMessagingConfiguration {
 
    private SimpleMessageChannel simpleMessageChannel;
 
-    private final Flux<Message<String>> simpleMessageSupplier;
-
-
-    public OutputMessagingConfiguration() {
-         simpleMessageSupplier = Flux.create(sink -> simpleMessageChannel = sink::next);
-    }
-
     @Bean
     public SimpleMessageChannel simpleMessageChanel() {
         return d -> simpleMessageChannel.accept(d);
@@ -41,49 +34,12 @@ public class OutputMessagingConfiguration {
 
     @Bean
     public Supplier<Flux<Message<String>>> simpleMessageProducer() {
-        return () -> simpleMessageSupplier;
+        return () -> Flux.create(sink -> simpleMessageChannel = sink::next);
     }
 
-//
-//    Random random = new Random();
-
-//    @Bean
-//    public Function<String, Message<String>> simpleMessageProducer() {
-//        return message -> {
-//            return MessageBuilder.withPayload(message).build();
-//        };
-//    }
-
-//    @Bean
-//    public Function<String, Flux<Integer>> simpleMessageProducer() {
-//        return (a) -> Flux.interval(Duration.ofSeconds(5)).map(value -> random.nextInt(1000 - 1) + 1).log();
-//    }
-
-//    @Bean
-//    public Consumer<Message<String>> simpleMessageReceiver() {
-//        return message -> {
-//            log.info("");
-//        };
-//    }
-
-//    @Bean
-//    public Function<Flux<Integer>, Flux<String>> fizzBuzzProcessor(){
-//        return longFlux -> longFlux
-//                .map(i -> evaluateFizzBuzz(i))
-//                .log();
-//    }
-//
-//
-//    private String evaluateFizzBuzz(Integer value) {
-//        if (value % 15 == 0) {
-//            return "FizzBuzz";
-//        } else if (value % 5 == 0) {
-//            return "Buzz";
-//        } else if (value % 3 == 0) {
-//            return "Fizz";
-//        } else {
-//            return String.valueOf(value);
-//        }
-//    }
+    @Bean
+    public Consumer<Message<String>> simpleMessageReceiver() {
+        return message -> log.info(message.getPayload());
+    }
 
 }
