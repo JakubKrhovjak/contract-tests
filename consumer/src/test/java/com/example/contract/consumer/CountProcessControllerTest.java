96d198dc80603c47aecf468dd856800afa341245
Jakub Krhovjak <jakub.krhovjak@protonmail.com>
2022-09-24 10:24:26 +0200

    consumer message test

--- consumer/src/test/java/com/example/contract/consumer/CountProcessControllerTest.java
+++ consumer/src/test/java/com/example/contract/consumer/CountProcessControllerTest.java
@@ -1,24 +1,35 @@
 package com.example.contract.consumer;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
+import org.springframework.cloud.stream.binder.test.InputDestination;
+import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
 import org.springframework.http.MediaType;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.nio.charset.StandardCharsets;
 
 /**
  * Created by Jakub Krhovják on 9/22/22.
  */
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest( classes = { ConsumerApplication.class, TestChannelBinderConfiguration.class },
+                 webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 @AutoConfigureJsonTesters
 @AutoConfigureStubRunner(
@@ -30,6 +41,9 @@ public class CountProcessControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private InputDestination input;
+
     @Test
     public void given_WhenPassEvenNumberInQueryParam_ThenReturnEven()
             throws Exception {
@@ -39,4 +53,15 @@ public class CountProcessControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().string("Even"));
     }
+
+    @Test
+    @ExtendWith(OutputCaptureExtension.class)
+    public void inputMessageTest(CapturedOutput log) throws Exception {
+        input.send(MessageBuilder.withPayload("test").build(),"consumer-event.destination");
+
+        assertThat(log.getOut()).contains("Data: test");
+    }
+
+
+
 }
